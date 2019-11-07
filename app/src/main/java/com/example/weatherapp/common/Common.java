0204328fd0c54c13660090c49a39842574573b9f
Daniil-Mysnik <44915342+Daniil-Mysnik@users.noreply.github.com>
2019-11-07 20:32:43 +0600

    first

---
+++ app/src/main/java/com/example/weatherapp/common/Common.java
@@ -0,0 +1,8 @@
+package com.example.weatherapp.common;
+
+import android.location.Location;
+
+public class Common {
+    public static final String APP_ID = "1499f87b39982a746c16f0c3ff09b18b";
+    public static Location current_location = null;
+}
