21d87a2d86155e1b1991f3bdaf064c2f9fdcb598
Daniil-Mysnik <44915342+Daniil-Mysnik@users.noreply.github.com>
2019-11-08 13:00:47 +0600

    second

--- app/src/main/java/com/example/weatherapp/common/Common.java
+++ app/src/main/java/com/example/weatherapp/common/Common.java
@@ -2,7 +2,24 @@ package com.example.weatherapp.common;
 
 import android.location.Location;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Common {
     public static final String APP_ID = "1499f87b39982a746c16f0c3ff09b18b";
     public static Location current_location = null;
+
+    public static String convertUnixToDate(int dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
+
+    public static String convertUnixToHour(int sunrise) {
+        Date date = new Date(sunrise*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm EEE MM yyyy");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
 }
